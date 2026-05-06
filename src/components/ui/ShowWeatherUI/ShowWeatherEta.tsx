d7a600452e86e4e3e9f06b92c0e46303059e82e8
funcnroll <115594042+funcnroll@users.noreply.github.com>
2026-05-06 11:08:42 +0200

    Adjust Eta formatting to be more precise

--- src/components/ui/ShowWeatherUI/ShowWeatherEta.tsx
+++ src/components/ui/ShowWeatherUI/ShowWeatherEta.tsx
@@ -3,10 +3,12 @@ function ShowWeatherETA({ eta }: { eta: Date }) {
     <div className="flex items-center justify-between px-1">
       <span className="text-xs text-zinc-500 uppercase tracking-wide">ETA</span>
       <span className="text-sm font-medium text-zinc-300">
-        {eta.toLocaleTimeString([], {
+        {new Intl.DateTimeFormat(navigator.language, {
+          day: "numeric",
+          month: "short",
           hour: "2-digit",
           minute: "2-digit",
-        })}
+        }).format(eta)}
       </span>
     </div>
   );
