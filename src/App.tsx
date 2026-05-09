940a71796a9316049eee508a35a210a9c4b2c259
funcnroll <115594042+funcnroll@users.noreply.github.com>
2026-05-09 11:07:16 +0200

    Simplify xl: class in App <div>

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,7 @@ function AppInner() {
         </MapContainer>
       </div>
 
-      <div className="w-full lg:w-96 xl:w-[28rem] bg-zinc-800 border-t lg:border-t-0 lg:border-l border-zinc-700 p-4 text-white lg:overflow-y-auto lg:h-screen">
+      <div className="w-full lg:w-96 xl:w-md bg-zinc-800 border-t lg:border-t-0 lg:border-l border-zinc-700 p-4 text-white lg:overflow-y-auto lg:h-screen">
         <GpxInput />
         <DepartureTime />
         <RiderProfile />
